292ba26557ea7dca8f30e9cd3838cadb983baf2b
LaupWing <loc_strict@hotmail.com>
2020-03-25 23:50:48 +0100

    update: refactored oauth and added middleware

--- server/routes/oauth/oauth.js
+++ server/routes/oauth/oauth.js
@@ -6,41 +6,43 @@ const router = express.Router()
 require("dotenv").config()
 
 
-let redirect_uri = 
+const redirect_uri = 
   process.env.REDIRECT_URI || 
   'http://localhost:8888/spotify/callback'
 
-router.get('/spotify/login', function(req, res) {
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-        response_type: 'code',
-        client_id: process.env.SPOTIFY_CLIENT_ID,
-        scope: 'user-read-private user-read-email',
-        redirect_uri
-    }))
-})
-
-router.get('/spotify/callback', function(req, res) {
-    let code = req.query.code || null
-    let authOptions = {
-        url: 'https://accounts.spotify.com/api/token',
-        form: {
-            code: code,
-            redirect_uri,
-            grant_type: 'authorization_code'
-        },
-        headers: {
-            'Authorization': 'Basic ' + (new Buffer.from(
-                process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
-            ).toString('base64'))
-        },
-        json: true
-    }
-    request.post(authOptions, function(error, response, body) {
-        var access_token = body.access_token
-        let uri = process.env.FRONTEND_URI || 'http://localhost:8888/'
-        req.session.acces_token = access_token
-        res.redirect(uri)
+router
+    .get('/spotify/login', function(req, res) {
+        res.redirect('https://accounts.spotify.com/authorize?' +
+            querystring.stringify({
+                response_type: 'code',
+                client_id: process.env.SPOTIFY_CLIENT_ID,
+                scope: 'user-read-private user-read-email',
+                redirect_uri
+            })
+        )
     })
-})
+    .get('/spotify/callback', function(req, res) {
+        const code = req.query.code || null
+        const authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (new Buffer.from(
+                    process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+                ).toString('base64'))
+            },
+            json: true
+        }
+        request.post(authOptions, function(error, response, body) {
+            const access_token = body.access_token
+            const uri = process.env.FRONTEND_URI || 'http://localhost:8888/'
+            req.session.acces_token = access_token
+            res.redirect(uri)
+        })
+    })
+
 module.exports = router
