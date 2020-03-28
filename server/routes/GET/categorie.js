80e1a38ab722c3ae0b64d52952cd1d6084ac7941
LaupWing <loc_strict@hotmail.com>
2020-03-28 16:04:04 +0100

    categorie update

--- server/routes/GET/categorie.js
+++ server/routes/GET/categorie.js
@@ -1,7 +1,11 @@
 const spotify = require('../../api/spotify')
+const config = require('../config')
 
 module.exports = async (req, res) => {
     const categorie = await spotify.getCategorie(req.session.acces_token, req.params.id)
-    console.log(categorie)
-    res.send('categorie')
+    console.log(categorie.playlists.items)
+    res.render(config.template, {
+        page: 'categorie',
+        playlists: categorie.playlists.items
+    })
 }
