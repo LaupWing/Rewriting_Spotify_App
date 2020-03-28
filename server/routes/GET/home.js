614d177e216bd7fa3ccb357cc237bab17913cc45
LaupWing <loc_strict@hotmail.com>
2020-03-28 19:13:29 +0100

    base styling added

--- server/routes/GET/home.js
+++ server/routes/GET/home.js
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
     const categories = await spotify.getCategories(req.session.acces_token)
     const me = await spotify.getUserInfo(req.session.acces_token)
     let user = await User.findOne({ spotify_id: me.id })
-
+    console.log(categories.categories.items[0])
     if (!user) {
         user = new User({
             name: me.display_name,
