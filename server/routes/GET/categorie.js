3f2feed86604dda1b66bae150bc8861cd9c9ee18
LaupWing <loc_strict@hotmail.com>
2020-04-02 13:38:48 +0200

    added total display

--- server/routes/GET/categorie.js
+++ server/routes/GET/categorie.js
@@ -3,7 +3,7 @@ const config = require('../config')
 
 module.exports = async (req, res) => {
     const categorie = await spotify.getCategorie(req.session.acces_token, req.params.id)
-
+    console.log(categorie.playlists.items[0])
     res.render(config.template, {
         page: 'playlists',
         playlists: 'items' in categorie.playlists ? categorie.playlists.items : [],
