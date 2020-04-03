038bf60a427c33369691a7c89cda2fc3a54107db
LaupWing <loc_strict@hotmail.com>
2020-04-03 13:02:00 +0200

    removed the logs

--- server/routes/GET/categorie.js
+++ server/routes/GET/categorie.js
@@ -3,9 +3,7 @@ const config = require('../config')
 
 module.exports = async (req, res) => {
     const categorie = await spotify.getCategorie(req.session.acces_token, req.params.id)
-    console.log(req.session.user)
-    console.log(req.session.favorites)
-    console.log(req.session.records)
+
     res.render(config.template, {
         page: 'playlists',
         playlists: 'items' in categorie.playlists ? categorie.playlists.items : [],
